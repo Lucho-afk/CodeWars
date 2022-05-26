52611bb6ff32c3b5c3338ad0d41075500c78a330
Luciano Leone <lucianoleone2011@hotmail.es>
2022-05-26 10:35:05 -0300

    kata #16: I love you, a little , a lot, passionately ... not at all, by nbeck, 8 kyu

---
+++ kata #016-I love you, a little , a lot, passionately ... not at all-8kyu/src/Test/Sid.java
@@ -0,0 +1,7 @@
+package Test;
+public class Sid {
+    public static String howMuchILoveYou(int nb_petals) {
+    String [] sid={"I love you","a little","a lot","passionately","madly","not at all"};
+    return sid[nb_petals-1];
+    }
+}
