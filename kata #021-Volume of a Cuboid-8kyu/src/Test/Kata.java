e467cc539ad6ceae8e90bc831d12e72c3a9c82f9
Luciano Leone <lucianoleone2011@hotmail.es>
2022-05-31 18:30:37 -0300

    kata #21: Volume of a Cuboid, by user5196477, 8 kyu

---
+++ kata #021-Volume of a Cuboid-8kyu/src/Test/Kata.java
@@ -0,0 +1,9 @@
+package Test;
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+
+    return length*width*height;
+  }
+  
+}
