fd35554fea9564257c62b7d0b1e8e7de19a24e6f
Luciano Leone <lucianoleone2011@hotmail.es>
2022-06-02 19:00:24 -0300

    kata #23: Plural, by wthit56, 8 kyu

---
+++ kata #023-Plural-8kyu/src/Test/Plural.java
@@ -0,0 +1,6 @@
+package Test;
+public class Plural{
+	  public static boolean isPlural(float f){
+	    return !(f==1);
+	  }
+	}
---
+++ kata #023-Plural-8kyu/src/Test/PluralTest.java
@@ -0,0 +1,18 @@
+package Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class PluralTest {
+   @Test
+    public void BasicTest() {
+      assertEquals(true,Plural.isPlural(0f));
+      assertEquals(true,Plural.isPlural(0.5f));
+      assertEquals(false,Plural.isPlural(1f));
+      assertEquals(true,Plural.isPlural(100f));
+    }
+}
