ca816ef09bf0851bc45b92b9529b98dcfb0b3fe3
Luciano Leone <lucianoleone2011@hotmail.es>
2022-06-25 22:37:29 -0300

    kata #46: Convert a Boolean to a String, by btaitelb, 8 kyu

---
+++ kata #046-Convert a Boolean to a String-8kyu/src/Test/BoolToStrTest.java
@@ -0,0 +1,13 @@
+package Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BoolToStrTest{
+  @Test public void testTrue(){
+    assertEquals(BooleanToString.convert(true), "true");
+  }
+  
+  @Test public void testFalse(){
+    assertEquals(BooleanToString.convert(false), "false");
+  }
+}
---
+++ kata #046-Convert a Boolean to a String-8kyu/src/Test/BooleanToString.java
@@ -0,0 +1,6 @@
+package Test;
+public class BooleanToString {
+  public static String convert(boolean b){
+    return (b)?"true":"false";
+  }
+}
